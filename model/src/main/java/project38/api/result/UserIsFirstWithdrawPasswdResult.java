9f6d660bc98a058320db6af5c24465d95f9f23af
CMS-CPXITONG\Administrator <cpxitongcms@hotmail.com>
2017-07-15 11:51:22 +0800

    add:用户是否第一次设置取款密码

---
+++ model/src/main/java/project38/api/result/UserIsFirstWithdrawPasswdResult.java
@@ -0,0 +1,18 @@
+package project38.api.result;
+
+import project38.api.common.result.CommonResult;
+
+/**
+ * 查询用户是否是第一次设置取款密码，（TRUE为第一次）
+ */
+public class UserIsFirstWithdrawPasswdResult extends CommonResult{
+    public Boolean userIsFirstWithdrawPasswd;
+
+    public Boolean getUserIsFirstWithdrawPasswd() {
+        return userIsFirstWithdrawPasswd;
+    }
+
+    public void setUserIsFirstWithdrawPasswd(Boolean userIsFirstWithdrawPasswd) {
+        this.userIsFirstWithdrawPasswd = userIsFirstWithdrawPasswd;
+    }
+}
