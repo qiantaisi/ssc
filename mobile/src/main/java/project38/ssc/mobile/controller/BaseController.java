fa33f7b03d8c113a3c4ef3cb6cd44ec085c1036a
cpxitong <cpxitong@gmail.com>
2017-08-03 09:59:41 +0800

    FIX

--- mobile/src/main/java/project38/ssc/mobile/controller/BaseController.java
+++ mobile/src/main/java/project38/ssc/mobile/controller/BaseController.java
@@ -74,27 +74,37 @@ public abstract class BaseController {
         request.setAttribute("commonResPath", RequestUtils.getBasePath(httpServletRequest) + "static/common/");
         request.setAttribute("bottomCssResPath", RequestUtils.getBasePath(httpServletRequest) + "static/public/theme/");
 
-        MobileFgResult mobileFgResult = ApiUtils.getMobileFg(companyShortName);
-        request.setAttribute("themeBottomNav", mobileFgResult.mobileFg.getDibu());
+        // 非JSON请求，需要加载模板风格
+        if (httpServletRequest.getRequestURI().indexOf(".json") < 0) {
+            MobileFgResult mobileFgResult = ApiUtils.getMobileFg(companyShortName);
+            request.setAttribute("themeBottomNav", mobileFgResult.mobileFg.getDibu());
+        }
 
         Long uid = this.getUid(httpServletRequest);
         String token = this.getToken(httpServletRequest);
-        UserSessionResult userSessionResult = ApiUtils.getUserSession(uid, token, companyShortName);
-        if (null != userSessionResult && userSessionResult.getResult() == 1) {
-            modelMap.put("userSession", userSessionResult);
+        try {
+            UserSessionResult userSessionResult = ApiUtils.getUserSession(uid, token, companyShortName);
+            if (null != userSessionResult && userSessionResult.getResult() == 1) {
+                modelMap.put("userSession", userSessionResult);
+            }
+        } catch (Exception e) {
+            log.error(this, e);
         }
 
-        // 公共模板读取风格
-        FenggeResult fenggeResult = ApiUtils.getWebFengge(
-                companyShortName,
-                2
-        );
+        // 非JSON请求，需要加载模板风格
+        if (httpServletRequest.getRequestURI().indexOf(".json") < 0) {
+            // 公共模板读取风格
+            FenggeResult fenggeResult = ApiUtils.getWebFengge(
+                    companyShortName,
+                    2
+            );
 
-        if (null == fenggeResult || fenggeResult.getResult() != 1) {
-            throw new UserException(-997, "服务器错误");
+            if (null == fenggeResult || fenggeResult.getResult() != 1) {
+                throw new UserException(-997, "服务器错误");
+            }
+            modelMap.put("fengge_1", fenggeResult.getFengge_1());
+            modelMap.put("fengge_2", fenggeResult.getFengge_2());
         }
-        modelMap.put("fengge_1", fenggeResult.getFengge_1());
-        modelMap.put("fengge_2", fenggeResult.getFengge_2());
 
         ModelAndView modelAndView = new ModelAndView("theme/" + theme + "/" + jspLocation);
         modelAndView.addAllObjects(modelMap);
@@ -207,8 +217,12 @@ public abstract class BaseController {
         httpServletRequest.setAttribute("commonResPath", RequestUtils.getBasePath(httpServletRequest) + "static/common/");
         httpServletRequest.setAttribute("bottomCssResPath", RequestUtils.getBasePath(httpServletRequest) + "static/public/theme/");
 
-        MobileFgResult mobileFgResult = ApiUtils.getMobileFg(companyShortName);
-        httpServletRequest.setAttribute("themeBottomNav", mobileFgResult.mobileFg.getDibu());
+
+        // 非JSON请求，需要加载模板风格
+        if (httpServletRequest.getRequestURI().indexOf(".json") < 0) {
+            MobileFgResult mobileFgResult = ApiUtils.getMobileFg(companyShortName);
+            httpServletRequest.setAttribute("themeBottomNav", mobileFgResult.mobileFg.getDibu());
+        }
 
         Long uid = this.getUid(httpServletRequest);
         String token = this.getToken(httpServletRequest);
@@ -221,17 +235,21 @@ public abstract class BaseController {
             log.error(this, e);
         }
 
-        // 公共模板读取风格
-        FenggeResult fenggeResult = ApiUtils.getWebFengge(
-                companyShortName,
-                2
-        );
 
-        if (null == fenggeResult || fenggeResult.getResult() != 1) {
-            throw new UserException(-997, "服务器错误");
+        // 非JSON请求，需要加载模板风格
+        if (httpServletRequest.getRequestURI().indexOf(".json") < 0) {
+            // 公共模板读取风格
+            FenggeResult fenggeResult = ApiUtils.getWebFengge(
+                    companyShortName,
+                    2
+            );
+
+            if (null == fenggeResult || fenggeResult.getResult() != 1) {
+                throw new UserException(-997, "服务器错误");
+            }
+            modelMap.put("fengge_1", fenggeResult.getFengge_1());
+            modelMap.put("fengge_2", fenggeResult.getFengge_2());
         }
-        modelMap.put("fengge_1", fenggeResult.getFengge_1());
-        modelMap.put("fengge_2", fenggeResult.getFengge_2());
 
         ModelAndView modelAndView = new ModelAndView("public/" + jspLocation);
         modelAndView.addAllObjects(modelMap);
