6bed2a39ff1924b11d39f49154d0cb439220a644
cpxitong <cpxitong@gmail.com>
2017-08-17 15:27:17 +0800

    FIX

--- model/src/main/java/project38/api/result/UserSessionResult.java
+++ model/src/main/java/project38/api/result/UserSessionResult.java
@@ -1,13 +1,12 @@
 package project38.api.result;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import project38.api.common.result.CommonResult;
 import java.util.Date;
 
 import java.math.BigDecimal;
 
-/**
- * Created by Administrator on 2016/11/15.
- */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class UserSessionResult extends CommonResult {
     /**
      * 编码
